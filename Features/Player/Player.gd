36218f6a929045c9f453e52cfc4941ddd7aacf75
Reymo Je <rubicsoft0@gmail.com>
2026-09-12 04:53:08 +0700

    Improve kustomisasi Player movement + Controller support + Global singleton

--- Features/Player/Player.gd
+++ Features/Player/Player.gd
@@ -1,21 +1,45 @@
 extends CharacterBody2D
+class_name Player
 
+@export_group("Player Movement")
+@export_range(0.0, 1000.0, 0.1) var movement_speed := 300.0
+@export_range(0.0, 1000.0, 0.1) var jump_force := 400.0
+@export_range(0.0, 1.0, 0.01) var gravity_strength := 1.0
+@export_group("")
+@export_range(0, 256, 1) var camera_edge := 50
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@onready var cam_follow_pivot: Node2D = $CameraFreeTransform/CameraFollowPivot
+@onready var camera: Camera2D = $CameraFreeTransform/CameraFollowPivot/Camera2D
 
+var camera_follow := true
+
+func _enter_tree() -> void:
+	Global.player = self
+
+func _exit_tree() -> void:
+	Global.player = null
+
+func _ready() -> void:
+	cam_follow_pivot.global_position = global_position
+	camera.global_position = global_position
+
+func _process(delta: float) -> void:
+	if camera_follow: cam_follow_pivot.global_position = global_position - Vector2(0.0, camera_edge)
 
 func _physics_process(delta: float) -> void:
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-	
-	if Input.is_action_just_pressed("ui_accept"):
-		velocity.y = JUMP_VELOCITY
-	
-	var direction := Input.get_axis("ui_left", "ui_right")
+	# Movement handling
+	var direction := signf(Input.get_axis("move_left", "move_right"))
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * movement_speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, movement_speed)
+	
+	# Gravity calculation
+	if not is_on_floor():
+		velocity += get_gravity() * gravity_strength * delta
+	
+	# Jump mechanic
+	if Input.is_action_just_pressed("ui_accept"):
+		velocity.y = -jump_force
 
 	move_and_slide()
