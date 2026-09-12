6d22e61f5be2f34e02662280767479968a746f5f
Reymo Je <rubicsoft0@gmail.com>
2026-09-13 06:10:28 +0700

    High score on end game + default theme

--- UI/HUD/hud.gd
+++ UI/HUD/hud.gd
@@ -1,4 +1,11 @@
 extends CanvasLayer
 
+func _ready() -> void:
+	EventBus.connect("kill_player", func(_unused_var: bool) -> void:
+		$HUD_Box/Panel/ScoreCount.text = "High Score: " + str(Global.score)
+		$HUD_Box/Panel/AnimationPlayer.play("endgame")
+	)
+
 func _process(_delta: float) -> void:
-	$HUD_Box/ScoreCount.text = "Score: " + str(Global.score)
+	if Global.update_score:
+		$HUD_Box/Panel/ScoreCount.text = "Score: " + str(Global.score)
