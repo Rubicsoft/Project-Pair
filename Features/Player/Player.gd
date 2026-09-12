dfaeae12220314e797e011b6d798c752f4efa738
Reymo Je <rubicsoft0@gmail.com>
2026-09-12 16:06:29 +0700

    Smoothed movement + Upward burst + Sprite flip

--- Features/Player/Player.gd
+++ Features/Player/Player.gd
@@ -2,9 +2,11 @@ extends CharacterBody2D
 class_name Player
 
 @export_group("Player Movement")
-@export_range(0.0, 1000.0, 0.1) var movement_speed := 300.0
-@export_range(0.0, 1000.0, 0.1) var jump_force := 400.0
+@export_range(0.0, 1000.0, 0.1) var movement_speed := 200.0
 @export_range(0.0, 1.0, 0.01) var gravity_strength := 1.0
+@export_range(0.0, 1000.0, 0.1) var upward_force := 400.0
+@export_range(0.0, 10.0, 0.1)var upward_burst_duration := 0.5
+@export_range(1.0, 20.0, 0.1) var movement_smooth_vector := 4.0
 @export_group("")
 @export_range(0, 256, 1) var camera_edge := 50
 
@@ -14,6 +16,11 @@ class_name Player
 var last_ypos := 0.0
 var current_ypos := 0.0
 var camera_follow := true
+var upward_cooldown := 0.0
+var direction := 0.0
+var last_direction := 0.0
+var smoothed_direction := 0.0
+
 
 func _enter_tree() -> void:
 	Global.player = self
@@ -28,29 +35,49 @@ func _ready() -> void:
 	camera.global_position = global_position
 	
 	last_ypos = global_position.y
+	upward_cooldown = upward_burst_duration
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if camera_follow: cam_follow_pivot.global_position.y = global_position.y - camera_edge
+	
+	$Sprite2D.flip_h = last_direction > 0.0
 	
 	if Global.update_score:
 		current_ypos = global_position.y
-		Global.score = last_ypos - current_ypos
+		Global.score = int(last_ypos - current_ypos)
 
 func _physics_process(delta: float) -> void:
 	# Movement handling
-	var direction := signf(Input.get_axis("move_left", "move_right"))
+	direction = signf(Input.get_axis("move_left", "move_right"))
+	smoothed_direction = lerpf(smoothed_direction, direction, delta * movement_smooth_vector)
 	if direction:
-		velocity.x = direction * movement_speed
+		last_direction = direction
+		if is_on_floor():
+			velocity.x = direction * movement_speed
+		else:
+			velocity.x = smoothed_direction * movement_speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, movement_speed)
+		if is_on_floor():
+			velocity.x = move_toward(velocity.x, 0, movement_speed)
+		else:
+			velocity.x = move_toward(velocity.x, 0, delta * movement_speed)
 	
 	# Gravity calculation
 	if not is_on_floor():
 		velocity += get_gravity() * gravity_strength * delta
-	
-	# Jump mechanic
-	if Input.is_action_just_pressed("ui_accept"):
-		velocity.y = -jump_force
+
+	# Upward burst mechanic
+	if is_on_ceiling() or is_on_floor():
+		upward_force = 0.0
+		upward_cooldown = upward_burst_duration
+	if Input.is_action_pressed("ui_accept"):
+		upward_cooldown -= delta
+		if upward_cooldown > 0.0:
+			upward_force += 400.0 * delta
+			velocity.y = -upward_force
+	else:
+		upward_force = 0.0
+		upward_cooldown = upward_burst_duration
 
 	move_and_slide()
 
